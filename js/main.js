bb91e9bff1cf8342762a17fc2577a1f6f4b6cd22
PetejStone <petejstoney@gmail.com>
2018-04-30 16:18:08 -0700

    added slide in navigation

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 const navIcon = document.querySelector('#nav-icon');
 const testimony = document.querySelectorAll(".testimony");
 const user = document.querySelectorAll(".user");
-navIcon.addEventListener('click',()=> {
-  navIcon.classList.toggle("change");
-
-});
+const nav = document.querySelector('nav');
+const header = document.querySelector('header');
+const main = document.querySelector('main');
+const footer = document.querySelector('footer');
 
 
 //random user API
@@ -34,3 +34,15 @@ $.ajax({
 //--------------------------------------------------------------------------------------
 ////////////////////////End of Random User Generator///////////////////////
 //----------------------------------------------------------------------------------------
+
+//nav functionality
+navIcon.addEventListener('click',()=> {
+  navIcon.classList.toggle("change");
+
+  nav.classList.toggle('show');
+  header.classList.toggle('slide-left');
+  main.classList.toggle('slide-left');
+  footer.classList.toggle('slide-left');
+
+
+});
